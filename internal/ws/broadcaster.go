3b7e943ccea46657bdbeb6bb31326d6bca4eed80
Artem Lytkin <artemoid2007@gmail.com>
2026-08-19 21:40:20 +0300

    приборка бродкастера

--- internal/ws/broadcaster.go
+++ internal/ws/broadcaster.go
@@ -2,7 +2,7 @@ package ws
 
 import "github.com/google/uuid"
 
-// Broadcaster интерфейс для broadcast обновлений
+// Broadcaster - кто умеет рассылать обновления клиентам турнира
 type Broadcaster interface {
 	Broadcast(tournamentID uuid.UUID, messageType string, payload any)
 }
